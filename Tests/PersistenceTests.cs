b16de5467936bb741599f15f7036291afee1a7c7
Carlos Peix <carlos.peix@gmail.com>
2025-03-08 19:37:56 -0300

    Agrego una prueba para el cambio de valor de la configuración y paso las descripciones a español.

--- Tests/PersistenceTests.cs
+++ Tests/PersistenceTests.cs
@@ -22,13 +22,13 @@ public class PersistenceTests
     }
 
     [Test]
-    public void ConfigurationsIEmpty()
+    public void IniciaConCeroConfiguraciones()
     {
         Assert.That(_context.Configurations.Count(), Is.EqualTo(0));
     }
 
     [Test]
-    public void ShouldFailBecauseOfConfigurationNameDuplicated()
+    public void NombreDeConfiguracionDuplicadoDeberíaFallar()
     {
         _context.Configurations.Add(new Configuration("OneName", "one value"));
         _context.SaveChanges();
@@ -37,5 +37,28 @@ public class PersistenceTests
             _context.Configurations.Add(new Configuration("OneName", "another value"));
             _context.SaveChanges();
         });
+    }
+
+    [Test]
+    public void PuedeModificarValorDeConfiguracion()
+    {
+        var config = new Configuration("OneName", "value");
+        _context.Configurations.Add(config);
+        _context.SaveChanges();
+        var id = config.Id;
+
+        _context.Dispose();
+        _context = new ApplicationContext();
+
+        config = _context.Configurations.Find(id);
+        config?.ChangeValue("new value");
+        _context.SaveChanges();
+
+        _context.Dispose();
+        _context = new ApplicationContext();
+
+        config = _context.Configurations.Find(id);
+
+        Assert.That(config?.Value, Is.EqualTo("new value"));
     }
 }
