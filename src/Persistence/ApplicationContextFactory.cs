b158043b6191452d564012a0edf367a102ca8994
Carlos Peix <carlos.peix@gmail.com>
2025-03-23 17:33:47 -0300

    Adding the ability for the tooling (migrations) to instanciate the data context

---
+++ src/Persistence/ApplicationContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence;
+
+public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+{
+    public ApplicationContext CreateDbContext(string[] args)
+    {
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+        var connectionString = config.GetConnectionString("SabadosTechHexagonal");
+        if (connectionString == null)
+            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=SabadosTechHexagonalData.mdf;Integrated Security=True";
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        optionsBuilder.LogTo(Console.WriteLine, [DbLoggerCategory.Database.Command.Name], LogLevel.Information)
+                      .EnableSensitiveDataLogging();
+        
+        return new ApplicationContext(optionsBuilder.Options);
+    }
+}
