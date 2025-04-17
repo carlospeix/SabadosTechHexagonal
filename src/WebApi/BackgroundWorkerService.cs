b71b3bc3423537d52058c04f01098a6aa7a2f477
Carlos Peix <carlos.peix@gmail.com>
2025-04-17 08:08:35 -0300

    Implementing worker for scheduled notifications, using application service.

--- src/WebApi/BackgroundWorkerService.cs
+++ src/WebApi/BackgroundWorkerService.cs
@@ -1,23 +1,32 @@
-﻿using System.Threading;
+﻿using Model.Ports.Driving;
 
 namespace WebApi;
 
 public class BackgroundWorkerService : BackgroundService
 {
     private readonly ILogger<BackgroundWorkerService> logger;
+    private readonly IServiceProvider serviceProvider;
 
-    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger, IServiceProvider serviceProvider)
     {
         this.logger = logger;
+        this.serviceProvider = serviceProvider;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            logger.LogInformation("Background worker running at {time}.", DateTimeOffset.Now);
+            logger.LogDebug("Background worker running at {time}.", DateTimeOffset.Now);
 
-            await Task.Delay(TimeSpan.FromMilliseconds(1000), stoppingToken);
+            // Resolve INotifications within a scope
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var notifications = scope.ServiceProvider.GetRequiredService<INotifications>();
+                notifications.SendScheduledNotifications();
+            }
+
+            await Task.Delay(TimeSpan.FromMilliseconds(2000), stoppingToken);
         }
     }
 
