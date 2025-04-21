3e72912782cd05f3fce8b6eed25014a0c49eecb1
Carlos Peix <carlos.peix@gmail.com>
2025-04-21 07:55:25 -0300

    Teachers multitenancy

---
+++ src/Persistence/Migrations/20250421105325_TeacherMultitenancy.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeacherMultitenancy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TenantId",
+                table: "Teachers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_TenantId",
+                table: "Teachers",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Teachers_TenantId",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Teachers");
+        }
+    }
+}
